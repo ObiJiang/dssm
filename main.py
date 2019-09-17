e1f9c7a3abcaebc71b103b1a79539a4169fb0e36
Yibo Jiang <jyb940413@gmail.com>
2019-09-16 21:09:10 -0400

    running with mnist dataset

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import torch
+import torchvision
 import numpy as np
+
 # single DSSM layer
 from single_layer import SingleLayerDSSMForMnist
 
 # misc
 from misc import AttrDict
+from tqdm import tqdm
+
+# set random seed
+random_seed = 1
+torch.manual_seed(random_seed)
 
 class DSSM():
 	def __init__(self):
 		config = AttrDict()
 
-		input_dims = [10, 10, 3]
+		input_dims = [28, 28, 1]
 		config.input_dims = np.array(input_dims)
 		config.num_layers = 2
 
@@ -19,8 +26,12 @@ class DSSM():
 		assert(len(config.strides) == config.num_layers)
 
 		# nps
-		config.nps = [4, 16]
+		config.nps = [1, 1]
 		assert(len(config.nps) == config.num_layers)
+
+		# dist thresholds
+		config.io_ths = [1, 1]
+		config.lateral_ths = [0, 4]
 
 		# set up number of units per layer
 		config.num_units = [config.input_dims]
@@ -38,8 +49,13 @@ class DSSM():
 		config.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 		# learning rate and step size
-		config.step_size = 0.00001
-		config.lr = 0.001
+		config.euler_lr = 0.2
+		config.lr = 1e-2
+		config.lr_floor=1e-4
+		config.decay = 2
+
+		# training 
+		config.num_epochs = 100
 
 		self.config = config
 
@@ -50,49 +66,74 @@ class DSSM():
 			mnist_config = AttrDict()
 			# layer-wise configs
 			mnist_config.layer_ind = layer_ind
+			mnist_config.stride = self.config.strides[layer_ind]
 			mnist_config.input_dims = self.config.num_units[layer_ind]
 			mnist_config.output_dims = self.config.num_units[layer_ind + 1]
 			mnist_config.stride = self.config.strides[layer_ind]
+			mnist_config.io_th = self.config.io_ths[layer_ind]
+			mnist_config.lateral_th = self.config.lateral_ths[layer_ind]
 
 			#network-wise config
 			mnist_config.network_config = self.config
 
 			self.layers[layer_ind] = SingleLayerDSSMForMnist(mnist_config)
 
-	def run(self):
-		inp = torch.randn(10, device=self.config.device)
-		while True:
-			# neural dynamics loop
-			while True:
-				err = 0.0
-				for layer_ind in range(self.config.num_layers):
-					# set layer input
-					if layer_ind == 0:
-						cur_inp = inp
-					else:
-						cur_inp = self.layers[layer_ind - 1].get_output()
-
-					# set layer feedback input from top layer
-					if layer_ind == self.config.num_layers - 1:
-						cur_feedback = torch.zeros(self.config.hidden_units[-1])  
-					else: 
-						cur_feedback = self.layers[layer_ind + 1].feedback() 
-					
-					err += self.layers[layer_ind].dynamics(cur_inp, cur_feedback)
-
-				if err < 1e-5:
-					break
+	def single_pass(self, inp, epoch):
+		# neural dynamics loop
+		delta = np.ones(self.config.num_layers) * np.inf
+		for update_step in range(3000):
 
 			for layer_ind in range(self.config.num_layers):
+				# set layer input
 				if layer_ind == 0:
 					cur_inp = inp
 				else:
 					cur_inp = self.layers[layer_ind - 1].get_output()
-				self.layers[layer_ind].plasticity_update(cur_inp)
+
+				# set layer feedback input from top layer
+				if layer_ind == self.config.num_layers - 1:
+					cur_feedback = torch.zeros(int(np.prod(self.config.num_units[-1])))
+				else: 
+					cur_feedback = self.layers[layer_ind + 1].feedback() 
+				
+				delta[layer_ind] = self.layers[layer_ind].dynamics(cur_inp, cur_feedback, update_step)
+
+			# Hugo's parameter
+			if delta.any() < 1e-4:
+				break
+
+		for layer_ind in range(self.config.num_layers):
+			if layer_ind == 0:
+				cur_inp = inp
+			else:
+				cur_inp = self.layers[layer_ind - 1].get_output()
+			self.layers[layer_ind].plasticity_update(cur_inp, epoch)
+
+		return delta.sum()
+
+	def run(self):
+		# mnist data lodaer
+		train_loader = torch.utils.data.DataLoader(
+		  torchvision.datasets.MNIST('./data', train=True, download=True,
+		                             transform=torchvision.transforms.Compose([
+		                               torchvision.transforms.ToTensor(),
+		                               torchvision.transforms.Normalize(
+		                                 (0.1307,), (0.3081,))
+		                             ])),
+		  batch_size=1, shuffle=True)
+
+		for epoch in tqdm(range(self.config.num_epochs)):
+			loss = 0
+			for image, _ in train_loader:
+				image = image.to(self.config.device)
+				image = image.view(-1)
+				loss += self.single_pass(image, epoch)
+
+			print("{:} Epoch: loss {:}".format(epoch, loss))
 
 model = DSSM()
-# model.create_network()
-# model.run()
+model.create_network()
+model.run()
 
 
 
--- single_layer.py
+++ single_layer.py
@@ -1,55 +1,168 @@
 import torch
+import numpy as np
+import itertools
 
 class SingleLayerDSSMForMnist():
+	"""
+	Note: All the weights are reshaped into 1-D vector for calculation,
+	but the connectivity is specified by c_W_hat and c_L_hat (1/c for non-zero elements)
+	"""
 	def __init__(self, config):
 		self.input_dims = config.input_dims
-		self.ouput_dims = config.hidden_dims
+		self.input_linear_dim = int(np.prod(self.input_dims))
+		self.output_dims = config.output_dims
+		self.output_linear_dim = int(np.prod(self.output_dims))
+
 		self.layer_ind = config.layer_ind
+		self.stride = config.stride
+		self.io_th = config.io_th
+		self.lateral_th = config.lateral_th
 
 		self.network_config = config.network_config
 
+		# connectivies
+		self.c_W_hat = self.create_c_W() # this is supposed to be reciprocal
+		self.c_L_hat = self.create_c_L()
+
+		# Hugo's parameter -> factor
+		factor = np.sqrt(((np.sum(self.c_W_hat.data.numpy())/self.output_dims[-1])/np.prod(self.output_dims[:-1])))
+
 		# weights
-		self.W = torch.randn(input_dim, hidden_dim, device = self.network_config.device)
-		self.L = torch.randn(hidden_dim, hidden_dim, device = self.network_config.device)
+		self.W = torch.randn(self.output_linear_dim, self.input_linear_dim, 
+			device = self.network_config.device) / factor
+		self.L = torch.eye(self.output_linear_dim,
+		 	device = self.network_config.device)
+		# self.L = torch.randn(self.output_linear_dim, self.output_linear_dim,
+		# 	device = self.network_config.device)
 
 		# activations
-		self.u = torch.zeros(hidden_dim)
+		self.u = torch.zeros(self.output_linear_dim)
 		self.r = self.activation(self.u)
 
-		# connectivies
-		self.c_W_hat = torch.ones(input_dim, hidden_dim) # this is supposed to be reciprocal
-		self.c_L_hat = torch.ones(hidden_dim, hidden_dim)
-
 		# feedback_parameter
-		self.feedback_parameter = 0 if self.layer_ind == self.config.num_layers - 1 else 1
+		if self.layer_ind == self.network_config.num_layers - 1:
+			self.feedback_parameter = 0  
+		else: 
+			self.feedback_parameter = 1
 
 	def create_c_W(self):
-		pass
+		"""
+		compute once, so it may not be highly optimized.
+		the mask is the same for all channel pairs
+		"""
+		in_nps = self.input_dims[-1]
+		out_nps = self.output_dims[-1]
+		stride_padding = self.stride/2
+		fea_dim_ind = len(self.input_dims)-1
+
+		single_mask_dims = np.concatenate([self.output_dims[:-1], self.input_dims[:-1]])
+		c_W = torch.zeros(list(single_mask_dims))
+
+		# create indices matrix for easy dist calculation
+		input_indices = []
+		for dim_ind, dim in np.ndenumerate(self.input_dims[:-1]):
+			repeat_vec = (np.ones(len(self.input_dims)) * self.input_dims).astype(int)
+			repeat_vec[dim_ind] = 1
+			repeat_vec[-1] = 1
+
+			view_vec = np.ones(len(self.input_dims)).astype(int)
+			view_vec[dim_ind] = dim
+
+			indices_per_dim = torch.arange(dim).view(list(view_vec)).repeat(list(repeat_vec))
+			input_indices.append(indices_per_dim)
+
+		# Hugo Parameter -> 0.5
+		input_indices_tensor = torch.cat(input_indices, dim = fea_dim_ind) + 0.5
+		
+		# calculate C for one channel pair
+		for idx in itertools.product(*[range(s) for s in self.output_dims[:-1]]):
+			center = torch.tensor(idx) * self.stride + stride_padding
+			dist_mat = torch.norm(input_indices_tensor-center.float(), p=2, dim = fea_dim_ind)
+			c_W[idx] = (dist_mat <= self.io_th)
+
+		# replicate C for all channel pairs
+		nps_repeat_vec = np.ones(len(self.input_dims) + len(self.output_dims)).astype(int)
+		nps_repeat_vec[len(self.output_dims)-1] = out_nps
+		nps_repeat_vec[len(self.input_dims)-1 + len(self.output_dims)] = in_nps
+
+		nps_view_vec = np.concatenate([self.output_dims, self.input_dims]).astype(int)
+		nps_view_vec[len(self.output_dims)-1] = 1
+		nps_view_vec[len(self.input_dims)-1 + len(self.output_dims)] = 1
+		
+		c_W_tile = c_W.view(list(nps_view_vec)).repeat(list(nps_repeat_vec))
+		return c_W_tile.view([self.output_linear_dim, self.input_linear_dim])
+
 
 	def create_c_L(self):
-		pass
+		"""
+		compute once, so it may not be highly optimized
+		"""
+		out_nps = self.output_dims[-1]
+		stride_padding = self.stride/2
+		fea_dim_ind = len(self.output_dims)-1
 
-	def dynamics(self, prev_layer, feedback):
+		single_mask_dims = np.concatenate([self.output_dims[:-1], self.output_dims[:-1]])
+		c_L = torch.zeros(list(single_mask_dims))
+
+		# create indices matrix for easy dist calculation
+		output_indices = []
+		for dim_ind, dim in np.ndenumerate(self.output_dims[:-1]):
+			repeat_vec = (np.ones(len(self.output_dims)) * self.output_dims).astype(int)
+			repeat_vec[dim_ind] = 1
+			repeat_vec[-1] = 1
+
+			view_vec = np.ones(len(self.output_dims)).astype(int)
+			view_vec[dim_ind] = dim
+
+			indices_per_dim = torch.arange(dim).view(list(view_vec)).repeat(list(repeat_vec))
+			output_indices.append(indices_per_dim)
+
+		output_indices_tensor = torch.cat(output_indices, dim = fea_dim_ind)
+
+		for idx in itertools.product(*[range(s) for s in self.output_dims[:-1]]):
+			center = torch.tensor(idx)
+			dist_mat = torch.norm(output_indices_tensor-center.float(), p=2, dim = fea_dim_ind)
+			c_L[idx] = (dist_mat <= self.lateral_th)
+
+		# replicate C for all channel pairs
+		nps_repeat_vec = np.ones(len(self.output_dims) + len(self.output_dims)).astype(int)
+		nps_repeat_vec[len(self.output_dims)-1] = out_nps
+		nps_repeat_vec[len(self.output_dims)-1 + len(self.output_dims)] = out_nps
+
+		nps_view_vec = np.concatenate([self.output_dims, self.output_dims]).astype(int)
+		nps_view_vec[len(self.output_dims)-1] = 1
+		nps_view_vec[len(self.output_dims)-1 + len(self.output_dims)] = 1
+		
+		c_L_tile = c_L.view(list(nps_view_vec)).repeat(list(nps_repeat_vec))
+		return c_L_tile.view([self.output_linear_dim, self.output_linear_dim])
+
+	def dynamics(self, prev_layer, feedback, step):
 		r_save = self.r.clone()
-		du = - self.u + self.W @ prev_layer - (self.L - torch.eye(self.hidden_dim)) @ self.r + feedback
-		self.u += self.config.step_size * du
+		du = - self.u + self.W @ prev_layer - (self.L - torch.eye(self.output_linear_dim)) @ self.r + feedback
+		
+		# Hugo's euler update rule
+		lr = max((self.network_config.euler_lr/(1+0.005*step)), 0.05)
+
+		self.u += lr * du
 		self.r = self.activation(self.u)
 
 		err = torch.dist(self.r, r_save, 2)
 		return err.item()
 
-	def plasticity_update(self, prev_layer):
-		update_step = self.config.lr * self.config.gamma ** (self.layer_ind - self.config.num_layers)
+	def plasticity_update(self, prev_layer,epoch):
+		lr = max(self.network_config.lr/(1+self.network_config.decay*epoch), self.network_config.lr_floor)
 
-		dW = update_step * (prev_layer.t() @ self.r * torch.sign(self.c_W_hat) - self.W * self.c_W_hat)
-		dL = update_step / 2 * (self.r.t() @ self.r * torch.sign(self.c_L_hat) 
-			- self.L * self.c_L_hat / (1 + self.config.gamma * self.feedback_parameter))
+		update_step = lr * self.network_config.gamma ** (self.layer_ind - self.network_config.num_layers)
+
+		dW = update_step * (torch.einsum('i,j->ij', self.r, prev_layer) * torch.sign(self.c_W_hat) - self.W * self.c_W_hat)
+		dL = update_step / 2 * (torch.einsum('i,j->ij', self.r, self.r) * torch.sign(self.c_L_hat) 
+			- self.L * self.c_L_hat / (1 + self.network_config.gamma * self.feedback_parameter))
 
 		self.W += dW
 		self.L += dL
 
 	def feedback(self):
-		return self.config.gamma * self.W @ self.r
+		return self.network_config.gamma * self.W @ self.r
 
 	def activation(self, u):
 		r = torch.max(torch.min(u, torch.ones_like(u)), torch.zeros_like(u))
